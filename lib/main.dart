8ed9f09cd248a5d16c0745abace6fec689d42533
Ittalian <bothmather@au.com>
2024-03-05 02:05:40 +0900

    add playMusic method

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   dynamic responseState;
 
-  Future<void> getAccessToken() async {
-    final url = Uri.parse('https://accounts.spotify.com/api/token');
-    final response = await http.post(url, headers: {
+  // アクセストークンの取得
+  Future<void> playMusic(int index) async {
+    final accessTokenUrl = Uri.parse('https://accounts.spotify.com/api/token');
+    final accessTokenResponse = await http.post(accessTokenUrl, headers: {
       'Authorization':
           'Basic ZDMxNGNhZjdhODIwNDlmZWIwYzgwYzZiOTIzZGNkODA6YzUxOTU3ZmNiYWVhNDAwMDhlYjRkOGJhYzgwMzQwYmU=',
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -42,9 +43,28 @@ class _MyHomePageState extends State<MyHomePage> {
       'refresh_token':
           'AQDgisALHNFTFI2N1Rlj9_x1XgJdt6sB3wQXHDsvJtgBObpRvAD4O5bx1RjCIeBzA3T4wLSuoFyNyL0fsAvC3aBioPZxyfx8N7bwT6rn4J51V5crgBLdkWQdqfBwhBbd5HA',
     });
-    final accessToken = jsonDecode(response.body);
+
+    final accessToken = jsonDecode(accessTokenResponse.body)['access_token'];
+    final playMusicUrl = Uri.parse(
+        'https://api.spotify.com/v1/me/player/play?device_id=4bb8a462453c69392da0985c0084e61c96dadbc7');
+    final playMusicBody = jsonEncode({
+      'context_uri': 'spotify:playlist:6OJHpbVGr7JigmFEU9xq0O',
+      'offset': {
+        'position': index,
+      },
+      'position_ms': 0,
+    });
+    final playMusicResponse = await http.put(
+      playMusicUrl,
+      headers: {
+      'Authorization': 'Bearer ${accessToken.toString()}',
+      'Content-Type': 'application/json',
+      },
+      body: playMusicBody
+    );
+
     setState(() {
-      responseState = accessToken;
+      responseState = playMusicResponse;
     });
   }
 
@@ -58,12 +78,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Text(
-              responseState == null ? 'Outputed Response' : responseState['access_token'],
+              responseState == null
+                  ? ''
+                  : "completed!",
               style: const TextStyle(fontSize: 30),
             ),
             ElevatedButton(
-              onPressed: getAccessToken,
-              child: const Text('APIを叩くよ'),
+              onPressed: () => playMusic(3),
+              child: const Text('Push to play music!'),
             )
           ],
         ),
